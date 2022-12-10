38c5bc1fd13b38223ef3d22fb2cb3c015109c8e3
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2022-12-10 19:55:16 -0300

    Calendar Object finally done
    I'm not decided at all if I want year to be an object or an array, but it's finally done and I'm okay with the result

--- assets/js/calendary.js
+++ assets/js/calendary.js
@@ -1,97 +1,48 @@
+date = new Date()
 
-let January =[]
-let Febuary = []
-let March = []
-let April = []
-let May = []
-let June = []
-let July = []
-let August = []
-let September = []
-let October = []
-let November = []
-let Decembe = []
-
-let days = [
-    "Sabado",
-    "Domingo",
-    "Lunes",
-    "Martes",
-    "Miercoles",
-    "Jueves",
-    "Viernes"
+let week = [
+    "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday"
 ]
-var daysCount = 0
-const createCalendary = () => {
-    for (daysCount = daysCount; daysCount < 31; daysCount++) {
-        let index = daysCount - (Math.floor(daysCount / 7) * 7)
-        January.push(days[index])
-    }
-    console.log(January)
-    daysCount = daysCount - (Math.floor(daysCount / 7) * 7)
-    for (daysCount = daysCount; daysCount < 31; daysCount++) {
-        let index = daysCount - (Math.floor(daysCount / 7) * 7)
-        Febuary.push(days[index])
-    }
-    console.log(Febuary)
 
-    daysCount = daysCount - (Math.floor(daysCount / 7) * 7)
-    for (daysCount = daysCount; daysCount < 34; daysCount++) {
-        let index = daysCount - (Math.floor(daysCount / 7) * 7)
-        March.push(days[index])
-    }
-    console.log(March)
+let days = []
 
-    daysCount = daysCount - (Math.floor(daysCount / 7) * 7)
-    for (daysCount = daysCount; daysCount < 36; daysCount++) {
-        let index = daysCount - (Math.floor(daysCount / 7) * 7)
-        April.push(days[index])
-    }
-    console.log(April)
-
-    daysCount = daysCount - (Math.floor(daysCount / 7) * 7)
-    for (daysCount = daysCount; daysCount < 32; daysCount++) {
-        let index = daysCount - (Math.floor(daysCount / 7) * 7)
-        May.push(days[index])
-    }
-    console.log(May)
-
-    daysCount = daysCount - (Math.floor(daysCount / 7) * 7)
-    for (daysCount = daysCount; daysCount < 34; daysCount++) {
-        let index = daysCount - (Math.floor(daysCount / 7) * 7)
-        June.push(days[index])
-    }
-    console.log(June)
-
-    daysCount = daysCount - (Math.floor(daysCount / 7) * 7)
-    for (daysCount = daysCount; daysCount < 37; daysCount++) {
-        let index = daysCount - (Math.floor(daysCount / 7) * 7)
-        July.push(days[index])
-    }
-    console.log(July)
-
-    daysCount = daysCount - (Math.floor(daysCount / 7) * 7)
-    for (daysCount = daysCount; daysCount < 33; daysCount++) {
-        let index = daysCount - (Math.floor(daysCount / 7) * 7)
-        August.push(days[index])
-    }
-    console.log(August)
-    
-    daysCount = daysCount - (Math.floor(daysCount / 7) * 7)
-    for (daysCount = daysCount; daysCount < 35; daysCount++) {
-        let index = daysCount - (Math.floor(daysCount / 7) * 7)
-        September.push(days[index])
-    }
-    console.log(September)
-    
-    daysCount = daysCount - (Math.floor(daysCount / 7) * 7)
-    for (daysCount = daysCount; daysCount < 31; daysCount++) {
-        let index = daysCount - (Math.floor(daysCount / 7) * 7)
-        October.push(days[index])
-    }
-    console.log(October)
+for (let daysCount = 0; daysCount <= 365; daysCount++) {
+    let index = daysCount - (Math.floor(daysCount / 7) * 7)
+    days.push(week[index])
 }
 
-createCalendary()
+console.log(days)
 
-date = new Date()
+let January = days.splice(0, 31)
+let Febuary = days.splice(0, 28)
+let March = days.splice(0, 31)
+let April = days.splice(0, 30)
+let May = days.splice(0, 31)
+let June = days.splice(0, 30)
+let July = days.splice(0, 31)
+let August = days.splice(0, 31)
+let September = days.splice(0, 30)
+let October = days.splice(0, 31)
+let November = days.splice(0, 30)
+let December = days.splice(0, 31)
+
+let year = {
+    "January": January,
+    "Febuary": Febuary,
+    "March": March,
+    "April": April,
+    "May": May,
+    "June": June,
+    "July": July,
+    "August": August,
+    "September": September,
+    "October": October,
+    "November": November,
+    "December": December
+}
