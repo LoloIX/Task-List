b2aa8962e12436992b25f5127ecda5b03c4b86d6
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2022-12-12 15:10:31 -0300

    Calendar previous and next button create

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -19,8 +19,6 @@ let currentYear = date.getFullYear()
 let currentDate = document.getElementById("CurrentDate")
 let tbody = document.getElementsByTagName("tbody")
 
-console.log(tbody)
-
 const monthLength = (month, year) => {
     return 32 - (new Date(year, month, 32)).getDate()
 }
@@ -54,4 +52,31 @@ const createCalendar = (month, year) => {
     }
 }
 
-createCalendar(11, 2022)
+const changeMonth = (bool) => {
+    let num = sign = newMonth = 0
+
+    if (bool) {
+        num = 11
+        sign = 1
+    } else {
+        sign = -1
+        newMonth = 11
+    }
+
+    if (currentMonth === num) {
+        currentYear += sign
+        currentMonth = newMonth
+    } else {
+        currentMonth += sign
+    }
+
+    let save = tbody[0].children[0]
+
+    tbody[0].innerHTML = ""
+
+    tbody[0].append(save)
+
+    createCalendar(currentMonth, currentYear)
+}
+
+createCalendar(currentMonth, currentYear)
