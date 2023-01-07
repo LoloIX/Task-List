128a558743e67c3948585186feb9b52c3f42cb82
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-01-06 23:18:48 -0300

    general styles

--- messages/src/index.js
+++ messages/src/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./messages"
-import "./styles.css"
-import "./side-bar.css"
-import "./menu-button.css"
+import "./css/styles.css"
+import "./css/side-bar.css"
+import "./css/menu-button.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
