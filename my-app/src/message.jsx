3a96fc8920cbb3fb203c025502164fa683aa9c9a
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-01-11 21:59:48 -0300

    user photo update
    actually, is not the real photo, but it's an example

--- my-app/src/message.jsx
+++ my-app/src/message.jsx
@@ -1,9 +1,14 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons"
 
 function Message(props) {
     return (
-        <div className="msg" id={props.id}>
-            <p>{props.name}</p>
+        <div>
+            <div className="msg" id={props.id}>
+                <p>{props.name}</p>
+            </div>
+            <FontAwesomeIcon icon={faCircleUser} />
         </div>
     )
 }
