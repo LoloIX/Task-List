493317f7ae14f359038bb3d257a837445af6bc47
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-01-13 22:45:58 -0300

    peerjs starts to working
    Actually it doesn't print anything, but it makes connections with others

--- my-app/src/chat.jsx
+++ my-app/src/chat.jsx
@@ -4,17 +4,51 @@ import Message from "./message"
 import { nanoid } from "nanoid"
 
 const DATA = []
+var peer = new Peer(null, {debug: 3})
 
 function Chat() {
+
+    const [inputLocalPeerId, setLocalValue] = React.useState("")
+    const [inputRemotePeerId, setRemoteValue] = React.useState("")
+    const [messages, setMessage] = React.useState(DATA)
+
+    peer.on('open', id => {
+        setLocalValue(id)
+        console.log("ID: " + peer.id)
+    })
+
+    peer.on('conection', (c) => {
+        c.on('open', () => {
+            console.log("somene connect me")
+            c.send("wolaaaa")
+        })
+
+        console.log("conected to: " + c.peer)
+
+        conn.on('data', (data) => {
+            console.log("Data recieved")
+            addMessage(data)
+        })
+    })
+
+    const SendMessage = () => {
+        const conn = peer.connect(inputRemotePeerId, {reliable: true})
+        conn.on('open', () => {
+            console.log("connected to: " + conn.peer)
+        })
+        conn.on('data', (data) => {
+            addMessage(data)
+        })
+    }
+
     const addMessage = (name) => {
+        console.log("im working")
         const newMessage = {id: `msg-${nanoid()}`, name}
         setMessage([...messages, newMessage])
         
         let elem = document.getElementsByClassName("messages")[0]
         elem.scroll(0, elem.scrollHeight)
     }
-
-    const [messages, setMessage] = React.useState(DATA)
 
     const messageList = messages.map((e, i) => { 
         DATA[i] = e
@@ -26,10 +60,29 @@ function Chat() {
         />
     )})
 
+    const handleOnChange = (elem) => {
+        setRemoteValue(elem.target.value)
+    }
+
     return (
         <div id="chat">
             <div className="messages">
                 {messageList}
+                <input 
+                    value={inputLocalPeerId}
+                />
+                <div>
+                    <input 
+                        value={inputRemotePeerId}
+                        onChange={handleOnChange}
+                    />
+
+                    <button
+                        onClick={SendMessage}
+                    >
+                        Connect
+                    </button>
+                </div>
             </div>
             
             <Form addMessage={addMessage}/>
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,9 +7,3 @@ import "./css/menu-button.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
-
-var peer = new Peer()
-
-peer.on("open", (id) => {
-    console.log("My peer ID is id: " + id)
-})
