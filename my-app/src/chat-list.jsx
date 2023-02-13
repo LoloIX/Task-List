f37ac439e4907a7b35ebe96498c1c6a7c487efb4
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-02-13 16:20:43 -0300

    the chats print in order and update well

--- my-app/src/chat-list.jsx
+++ my-app/src/chat-list.jsx
@@ -7,11 +7,70 @@ import { faCircleUser, faEllipsis, faUsers, faMagnifyingGlass } from "@fortaweso
 
 function ChatList(props) {
     const [showModal, setShow] = React.useState(false)
+    let chatsPrinted = []
 
-    const printChats = props.messagesStorage.map((e) => {
+    props.messagesStorage.map((e) => {
+        let repeat = false
+        let chat = {}
+    
+        if (e.group) {
+            chat.lastMessage = e.string
+            chat.lastMessageSender = (e.yours) ? "You" : e.sender
+            chat.title = e.groupName
+            chat.icon = faUsers
+
+            chatsPrinted.map((elem) => {
+                if (elem.title === e.groupName) {
+                    repeat = true
+                    elem.lastMessage = e.string
+                    elem.lastMessageSender = (e.yours) ? "You" : e.sender
+                }
+            })
+
+        } else {
+            if (e.yours) {
+                chat.lastMessage = e.string
+                chat.lastMessageSender = "You"
+                chat.title = e.receiver
+
+                chatsPrinted.map((elem) => {
+                    if (e.receiver === elem.title) {
+                        repeat = true
+                        elem.lastMessage = e.string
+                        elem.lastMessageSender = "You"
+                    }
+                })
+                
+            } else {
+                chat.lastMessage = e.string
+                chat.lastMessageSender = e.sender
+                chat.title = e.sender
+
+                chatsPrinted.map((elem) => {
+                    if (e.sender === elem.title) {
+                        repeat = true
+                        elem.lastMessage = e.string
+                        elem.lastMessageSender = e.sender
+                    }
+                })
+            }
+            chat.icon = faCircleUser
+        }
+
+        if (!repeat && chatsPrinted.length !== 0) {
+            chatsPrinted.push(chat)
+        } else if (chatsPrinted.length === 0) {
+            chatsPrinted.push(chat)
+        }
+    })
+
+    const printChats = chatsPrinted.map((e) => {
         return (
             <Chat
-                message={e}
+                icon={e.icon}
+                lastMessage={e.lastMessage}
+                lastMessageSender={e.lastMessageSender}
+                title={e.title}
                 key={`chat-${nanoid()}`}
             />
         )
--- my-app/src/chat.jsx
+++ my-app/src/chat.jsx
@@ -2,80 +2,18 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCircleUser, faUsers } from "@fortawesome/free-solid-svg-icons"
 
-let chatsPrintedStorage = []
-
 function Chat(props) {
-    const [chatsPrinted, addChat] = React.useState(chatsPrintedStorage)
-
-    let repeat = false
-    let chat = {}
-    
-    if (props.message.group) {
-        chat.lastMessage = props.message.string
-        chat.lastMessageSender = (props.message.yours) ? "You" : props.message.sender
-        chat.title = props.message.groupName
-        chat.icon = faUsers
-
-        chatsPrinted.map((e, i) => {
-            if (e.title === props.message.groupName) repeat = true
-            chat.lastMessageSender = (i + 1 === chatsPrinted.length && e.yours) ? "You" : e.sender
-            chat.lastMessage = e.string
-        })
-    } else {
-        if (props.message.yours) {
-            chat.lastMessage = props.message.string
-            chat.lastMessageSender = "You"
-            chat.title = props.message.receiver
-
-            chatsPrinted.map((e) => {
-                if (props.message.receiver === e.title) repeat = true
-                chat.lastMessage = e.string
-            })
-        } else {
-            chat.lastMessage = props.message.string
-            chat.lastMessageSender = props.message.sender
-            chat.title = props.message.sender
-
-            chatsPrinted.map((e) => {
-                if (props.message.sender === e.title) repeat = true
-                chat.lastMessage = e.string
-                chat.lastMessageSender = e.sender
-            })
-        }
-        chat.icon = faCircleUser
-    }
-
-    console.log(chatsPrintedStorage)
-    if (!repeat && chatsPrinted.length !== 0) {
-        chatsPrintedStorage.push(chat)
-        addChat([...chatsPrinted, chat])
-        return (
+    return (
+        <div>
             <div>
-                <div>
-                    <FontAwesomeIcon icon={chat.icon} />
-                </div>
-                <div>
-                    <h3>{chat.title}</h3>
-                    <p>{chat.lastMessageSender}: {chat.lastMessage}</p>
-                </div>
+                <FontAwesomeIcon icon={props.icon} />
             </div>
-        )
-    } else if (chatsPrinted.length === 0) {
-        chatsPrintedStorage.push(chat)
-        return (
             <div>
-                <div>
-                    <FontAwesomeIcon icon={chat.icon} />
-                </div>
-                <div>
-                    <h3>{chat.title}</h3>
-                    <p>{chat.lastMessageSender}: {chat.lastMessage}</p>
-                </div>
+                <h3>{props.title}</h3>
+                <p>{props.lastMessageSender}: {props.lastMessage}</p>
             </div>
-        )
-    }
-
-    return ""
+        </div>
+    )
 }
 
 export default Chat
